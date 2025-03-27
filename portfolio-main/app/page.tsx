2621448da8ba03fc06c9af6606bca8a32a2ce9b3
Aditya Bhadade <bhadadeaditya1310@gmail.com>
2025-03-28 03:05:29 +0530

    error fixedd

--- portfolio-main/app/page.tsx
+++ portfolio-main/app/page.tsx
@@ -8,13 +8,11 @@ import Footer from "@/components/Footer";
 import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
-import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
   return (
     <main className="relative bg-teal-800-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
